2669adff0fc1e229db0a138032fd73c414360314
initiatior <162955707+initiatior@users.noreply.github.com>
2024-03-25 12:13:33 +0400

    Add files via upload

---
+++ dachi_gobejishvili_1/tape_measure/sub/R21.java
@@ -0,0 +1,6 @@
+package quizes.q2.dachi_gobejishvili_1.tape_measure.sub;
+import quizes.q2.dachi_gobejishvili_1.thermodynamics.A22;
+        import quizes.q2.dachi_gobejishvili_1.tape_measure.A21
+public class R21 {
+            public static A22
+}
